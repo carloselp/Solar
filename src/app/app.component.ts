92418edd4c41fab971dad191435309a6218c1d4b
Kadu Portugal <cportugal@meucashcard.com.br>
2026-04-23 14:13:34 -0300

    fix: initialize pt-BR translations at app startup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { HttpLoadingService } from './services/http-loading.service';
+import { CoreService } from './services/core.service';
 
 @Component({
     selector: 'app-root',
@@ -12,4 +14,13 @@ import { HttpLoadingService } from './services/http-loading.service';
 export class AppComponent {
   title = 'InoveCode - InoveSolar';
   protected readonly httpLoadingService = inject(HttpLoadingService);
+
+  constructor() {
+      const translate = inject(TranslateService);
+      const coreService = inject(CoreService);
+      const language = coreService.getLanguage() || 'pt-BR';
+
+      translate.setDefaultLang(language);
+      translate.use(language);
+  }
 }
